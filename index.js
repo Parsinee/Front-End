4c3a9b94440b02d80be0d09c6d9eaa8fb6d92883
Parsinee <s6506021631026@email.kmutnb.ac.th>
2024-02-21 09:50:11 +0700

    .

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 var bodyParser = require('body-parser');
+const path = require("path");
 
 
 
@@ -13,12 +14,13 @@ var bodyParser = require('body-parser');
 const base_url = "http://node57019-parsinee-noderest.proen.app.ruk-com.cloud";
 
 
+
+app.set("views", path.join(__dirname, "/public/views"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app. use(express.static(__dirname + '/public'));
-
 
 app.get("/", async (req, res) => {
     try {
